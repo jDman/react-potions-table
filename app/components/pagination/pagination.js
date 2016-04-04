667ad2807392b4a71abe13d282b3e65dcdffa880
Jay Davis <davis.jaym@gmail.com>
2016-04-04 11:24:13 +0100

    Add component for page split

--- app/components/pagination/pagination.js
+++ app/components/pagination/pagination.js
@@ -1,27 +1,16 @@
 import React from 'react';
+import PageSelect from './pageSelect';
 
 class Pagination extends React.Component {
   constructor() {
     super();
   }
 
-  handleChange(event) {
-    this.props.updateResultsPerPage(event.target.value);
-  }
-
   render() {
     return (
       <form className="form-inline">
         <div className="form-group">
-          <select defaultValue="10"
-                  className="form-control"
-                  onChange={this.handleChange.bind(this)}
-                  ref="selectResults">
-            <option value="10">10</option>
-            <option value="25">25</option>
-            <option value="50">50</option>
-            <option value="Show all">Show all</option>
-          </select>
+          <PageSelect updateResultsPerPage={this.props.updateResultsPerPage}/>
 
           <span className="label label-default">page: {this.props.page}</span>
         </div>
